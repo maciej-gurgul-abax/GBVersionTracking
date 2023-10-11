1f33df38888ef53c3f3df2bf943bb78f53d8688f
Maciej Gurgul <maciej.gurgul@abax.com>
2023-10-11 22:10:49 +0200

    fix library

--- Package.swift
+++ Package.swift
@@ -1,22 +1,30 @@
-// swift-tools-version:5.0
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "GBVersionTracking",
-    platforms: [
-        .iOS(.v14),
-    ],
+    platforms: [.iOS(.v14)],
     products: [
-	.library(name: "GBVersionTracking",
-             targets: ["GBVersionTracking"])
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "GBVersionTracking",
+            targets: ["GBVersionTracking"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        .target(name: "GBVersionTracking",
-                path: "Sources/GBVersionTracking",
-                publicHeadersPath: "include",
-                cSettings: [
-                  .headerSearchPath("include"),
-                ]),
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "GBVersionTracking",
+            path: "Sources/GBVersionTracking",
+            publicHeadersPath: "include",
+            cSettings: [
+              .headerSearchPath("include"),
+            ]),
     ]
 )
