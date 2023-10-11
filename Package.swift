b3fb32801e19e26c109b88227da8e732599a1f4c
Maciej Gurgul <maciej.gurgul@abax.com>
2023-10-11 22:01:38 +0200

    fix spm support

--- Package.swift
+++ Package.swift
@@ -4,10 +4,18 @@ import PackageDescription
 
 let package = Package(
     name: "GBVersionTracking",
+    platforms: [
+        .iOS(.v14),
+    ],
     products: [
 	.library(name: "GBVersionTracking", targets: ["GBVersionTracking"])
     ],
     targets: [
-        .target(name: "GBVersionTracking", path: "GBVersionTracking")
+        .target(name: "GBVersionTracking",
+                path: "Sources/MGSwipeTableCell",
+                publicHeadersPath: "include",
+                cSettings: [
+                  .headerSearchPath("include"),
+                ]),
     ]
 )
